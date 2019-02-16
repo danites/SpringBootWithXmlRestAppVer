aa88dc4a0330901c1847950bfd7fd5b015dd4868
Daniel Kifle <kifledaniel7@gmail.com>
2019-02-16 15:27:36 -0600

    first commit

---
+++ src/main/java/com/infotech/app/service/MessageProviderService.java
@@ -0,0 +1,14 @@
+package com.infotech.app.service;
+
+public class MessageProviderService {
+
+	private String message;
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
